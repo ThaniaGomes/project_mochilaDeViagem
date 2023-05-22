e55cfda4d0953d7f36860e1a4aa07cd5c1661a8e
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2023-05-22 15:26:06 -0300

    add function criaElemento

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@ const form = document.getElementById("novoItem")
 
 form.addEventListener("submit", (evento)=> {
     evento.preventDefault()
-    console.log(evento)
 
-    console.log(evento.target.elements['nome'].value)
-    console.log(evento.target.elements['quantidade'].value)
+    criaElemento(evento.target.elements['nome'].value,
+    evento.target.elements['quantidade'].value)
 })
+
+function criaElemento(nome, quantidade){
+    console.log(nome)
+    console.log(quantidade)
+}
