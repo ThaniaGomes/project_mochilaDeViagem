cb016a8bc7f748c1f51893bd00a89c181aa45136
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2023-05-22 15:44:37 -0300

    add comments

--- js/main.js
+++ js/main.js
@@ -3,11 +3,20 @@ const form = document.getElementById("novoItem")
 form.addEventListener("submit", (evento)=> {
     evento.preventDefault()
 
-    criaElemento(evento.target.elements['nome'].value,
-    evento.target.elements['quantidade'].value)
+    criaElemento(evento.target.elements['nome'].value,evento.target.elements['quantidade'].value)
 })
 
-function criaElemento(nome, quantidade){
-    console.log(nome)
-    console.log(quantidade)
+function criaElemento(nome, quantidade){ 
+    //funcao responsavel por criar novos elementos na pagina
+    // <li class="item"><strong>7</strong>Camisas</li>
+
+    const novoIten = document.createElement('li') // cria uma li
+    novoIten.classList.add("item") // add class chamada item
+    const numeroItem = document.createElement('strong') // add strong com valor
+
+    numeroItem.innerHTML = quantidade // para o html receber a quantidade
+    novoIten.appendChild = numeroItem + nome //inserie um elemento criado dentro do outro
+    novoIten.innerHTML += nome // acresentar o nome do objeto ao inner
+
+    console.log(numeroItem)
 }
