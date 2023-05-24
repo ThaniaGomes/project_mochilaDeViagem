bdc6e1a4d66754f7f2d65a66d22fccd0da0061f0
Thânia Gomes <79340835+ThaniaGomes@users.noreply.github.com>
2023-05-24 15:45:08 -0300

    refatoração

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
 const itens = JSON.parse(localStorage.getItem("itens")) || [] 
-// ver se tem algo la no LS caso tenho pegar caso nao tenho retornar um array vazio
-//json parse usado pra transformar devolta em array que transformamos em string pro lS receber
 
 itens.forEach ((elemento) => {
     console.log(elemento.nome, elemento.quantidade)
@@ -10,40 +8,34 @@ itens.forEach ((elemento) => {
 
 form.addEventListener("submit", (evento)=> {
 
-    evento.preventDefault() //parar comportamento padrão do site para n voltar dados para propria pag.
+    evento.preventDefault() 
 
-    const nome = evento.target.elements['nome'] //chamar o elemento
-    const quantidade = evento.target.elements['quantidade'] //--
+    const nome = evento.target.elements['nome'] 
+    const quantidade = evento.target.elements['quantidade'] 
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    } 
 
-    criaElemento(nome.value, quantidade.value) //acessar o elemento
+    criaElemento(itemAtual) 
+    itens.push(itemAtual) 
+    localStorage.setItem("itens", JSON.stringify(itens)) 
 
-    nome.value = "" //limpa formulario
-    quantidade.value = "" //--
+    nome.value = "" 
+    quantidade.value = "" 
 })
 
-function criaElemento(nome, quantidade){ 
-    //funcao responsavel por criar novos elementos na pagina
-    // <li class="item"><strong>7</strong>Camisas</li>
+function criaElemento(item){ 
 
-    const novoIten = document.createElement("li") // cria uma li
-    novoIten.classList.add("item") // add class chamada item
-    const numeroItem = document.createElement("strong") // add strong com valor
+    const novoIten = document.createElement("li") 
+    novoIten.classList.add("item") 
+    const numeroItem = document.createElement("strong") 
 
-    numeroItem.innerHTML = quantidade // para o html receber a quantidade
-    novoIten.appendChild(numeroItem) //inserie um elemento criado dentro do outro responsavel pela bolinha com cor
-    novoIten.innerHTML += nome // acresentar o nome do objeto ao inner
+    numeroItem.innerHTML = item.quantidade 
+    novoIten.appendChild(numeroItem)
+    novoIten.innerHTML += item.nome 
 
     lista.appendChild(novoIten)
 
-    const itemAtual = {
-        "nome": nome,
-        "quantidade": quantidade
-    } //criaçao de cada novo item
-
-    itens.push(itemAtual) //inserir o item atual dentro do array itens
-
-    localStorage.setItem("itens", JSON.stringify(itens)) 
-    //salvar objeto transformado em string pelo json dentro do localStorage (array)
-    //{"nome":"camisa","quantidade":"1"}
-
 }
+
